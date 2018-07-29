97bfa1e944e3f03b060d63d8296cad5a46c65cd5
Michaela M <michaela.miller@outlook.com>
2018-07-29 17:43:31 -0600

    Can dynamically make calls using dropdown

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-function getWebcamAPIData() {
-  $('#country-list').on('change', 'select', event => {
-    let countryKeySelected = $('#country-select').val();
-  $.ajax({
-  url: 'https://webcamstravel.p.mashape.com/webcams/list/country=${countryKeySelected}',
-  data: {show:'webcams:image,location,player'},
-  orderby: 'popularity',
-  dataType: 'json',
-  type: 'GET',
-  headers: {"X-Mashape-Key": 'cgDecNwco7mshVPaItCd9ogfs5nnp1OCnOVjsn8Yn430nwywin',
-  "X-Mashape-Host": 'webcamstravel.p.mashape.com'},
-  success: function(data) {
-    console.log(data);
-    console.log(data.result.webcams["0"].title);
-    let webcamArray = data.result.webcams.map(function(webcam) {
-      return `
-      <img class="cam-results col-4" aria-label="Webcam Result" src="${webcam.player.day.embed}" 
-      height="200" width="400" style="border:none">`;
-    });
-    $('#js-cam-results').html(webcamArray.join(''));
-  }
-});
-});
-}
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const APIkey = 'AIzaSyAh6hD7Q7RpUpIMVwPvmRH-pH4nByXYHis';
 
 function displayList() {
   console.log('`displayList` ran');
@@ -37,25 +15,51 @@ function displayList() {
   $('#country-list').html(dropdownHtml);
 }
 
-
-
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-const APIkey = 'AIzaSyAh6hD7Q7RpUpIMVwPvmRH-pH4nByXYHis';
-
-function getDataFromAPI(callback) {
-  const settings = {
-    part: 'snippet',
-    key: APIkey,
-    q: 'Travel tips for Spain'
+function getWebcamAPIData() {
+  $('#country-list').on('change', 'select', event => {
+    let countryKeySelected = $('#country-select').val();
+    let countryNameSelected = $("#country-select").text();
+  $.ajax({
+  url: `https://webcamstravel.p.mashape.com/webcams/list/country=${countryKeySelected}`,
+  data: {show:'webcams:image,location,player'},
+  orderby: 'popularity',
+  dataType: 'json',
+  type: 'GET',
+  headers: {"X-Mashape-Key": 'cgDecNwco7mshVPaItCd9ogfs5nnp1OCnOVjsn8Yn430nwywin',
+  "X-Mashape-Host": 'webcamstravel.p.mashape.com'},
+  success: function(data) {
+    console.log(data);
+    console.log(data.result.webcams["0"].title);
+    let webcamArray = data.result.webcams.map(function(webcam) {
+      return `
+      <img width="400 height="200" src="${webcam.image.daylight.preview}">
+      `;
+    });
+    $('#js-cam-results').html(webcamArray.join(''));
   }
-  $.getJSON(YOUTUBE_SEARCH_URL, settings, callback);
+});
+});
 }
 
+  function getDataFromAPI(callback) {
+     $('#country-list').on('change', 'select', event => {
+        let countryNameSelected = $('#country-select').text();
+        const settings = {
+          part: 'snippet',
+          key: APIkey,
+          q: `Travel tips for ${countryNameSelected}`
+        };
+        $.getJSON(YOUTUBE_SEARCH_URL, settings, callback);
+        generateResult();
+      });
+}
+
+
 function generateResult(result) {
-  if(result.id.kind === "youtube#video") {
+  if(result.items.id.kind === "youtube#video") {
     return `
-    <img class="results" src="https://www.youtube.com/embed/${result.id.videoId}" height='200' width='400' title='${result.snippet.title}' aria-label='YouTube Video'> `;
-  } else if(result.id.kind === "youtube#channel") {
+    <iframe class="results" src="https://www.youtube.com/embed/${result.items.id.videoId}" height='200' width='400' title='${result.snippet.title}' aria-label='YouTube Video'></iframe>`;
+  } else if(result.items.id.kind === "youtube#channel") {
     return `
     <a href="https://www.youtube.com/user/${result.snippet.channelTitle}" target="_blank" rel="noopener" aria-label='YouTube Channel'><img class='results' src='${result.snippet.thumbnails.medium.url}' alt='${result.snippet.title}' height='200' width='400'></a>`;
   }
@@ -71,3 +75,12 @@ $(document).ready(function() {
   getWebcamAPIData();
   getDataFromAPI(displayYouTubeSearchData);
 });
+
+
+
+// <iframe class="cam-results col-4" aria-label="Webcam Result" src="${webcam.player.day.embed}" 
+// height="200" width="400" style="border:none"></iframe>
+
+ // <video width="400" height="200" controls>
+ //        <source src="${webcam.player.day.embed}">
+ //      </video>
