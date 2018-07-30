9bec0e5a25300d4b021ef411eb722f082692eb9e
Michaela M <michaela.miller@outlook.com>
2018-07-30 07:44:12 -0600

    Working youtube api call

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function displayList() {
   console.log(Object.keys(countryArray));
   const optionsHtml = Object.keys(countryArray).map((key, index) => {
     return `
-    <option class="country-list" value="${key}">${countryArray[key]}</option>`
+    <option class="country-list" value="${key}">${countryArray[key]}</option>`;
   });
 
   const dropdownHtml = `
@@ -27,39 +27,47 @@ function getWebcamAPIData() {
   type: 'GET',
   headers: {"X-Mashape-Key": 'cgDecNwco7mshVPaItCd9ogfs5nnp1OCnOVjsn8Yn430nwywin',
   "X-Mashape-Host": 'webcamstravel.p.mashape.com'},
-  success: function(data) {
-    console.log(data);
-    console.log(data.result.webcams["0"].title);
-    let webcamArray = data.result.webcams.map(function(webcam) {
-      return `
-      <img width="400 height="200" src="${webcam.image.daylight.preview}">
-      `;
-    });
-    $('#js-cam-results').html(webcamArray.join(''));
-  }
-});
+    success: function(data) {
+      console.log(data);
+      console.log(data.result.webcams["0"].title);
+      // console.log(webcam.location.city);
+      
+      let webcamArray = data.result.webcams.map(function(webcam) {
+        if (data.result.total > 0) { 
+        return `
+        <h3>${webcam.title}</h3>
+        <img width="400 height="200" src="${webcam.image.daylight.preview}">
+        `;
+        }
+      });
+      if(webcamArray.length > 0) {
+        $('#js-cam-results').html(webcamArray.join(''));
+      } else {
+        $('#js-cam-results').html("<p>This country has no webcams yet :( </p>");
+      }
+    }
+  });
 });
 }
 
   function getDataFromAPI(callback) {
      $('#country-list').on('change', 'select', event => {
-        let countryNameSelected = $('#country-select').text();
+        let countryNameSelected = $('#country-select option:selected').text();
         const settings = {
           part: 'snippet',
           key: APIkey,
           q: `Travel tips for ${countryNameSelected}`
         };
         $.getJSON(YOUTUBE_SEARCH_URL, settings, callback);
-        generateResult();
       });
 }
 
 
 function generateResult(result) {
-  if(result.items.id.kind === "youtube#video") {
+  if(result.id.kind === "youtube#video") {
     return `
-    <iframe class="results" src="https://www.youtube.com/embed/${result.items.id.videoId}" height='200' width='400' title='${result.snippet.title}' aria-label='YouTube Video'></iframe>`;
-  } else if(result.items.id.kind === "youtube#channel") {
+    <iframe class="results" src="https://www.youtube.com/embed/${result.id.videoId}" height='200' width='400' title='${result.snippet.title}' aria-label='YouTube Video'></iframe>`;
+  } else if(result.id.kind === "youtube#channel") {
     return `
     <a href="https://www.youtube.com/user/${result.snippet.channelTitle}" target="_blank" rel="noopener" aria-label='YouTube Channel'><img class='results' src='${result.snippet.thumbnails.medium.url}' alt='${result.snippet.title}' height='200' width='400'></a>`;
   }
