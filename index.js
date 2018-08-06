1005dfce138c752997bb6ec8e3b6d1384cfa4c61
Michaela M <michaela.miller@outlook.com>
2018-08-06 06:18:12 -0600

    Final clean-up for first submission

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-const APIkey = 'AIzaSyAh6hD7Q7RpUpIMVwPvmRH-pH4nByXYHis';
+const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
+const YOUTUBE_APIkey = "AIzaSyAh6hD7Q7RpUpIMVwPvmRH-pH4nByXYHis";
 
 //Create dropdown menu from store.js and render
 function displayList() {
@@ -10,23 +10,23 @@ function displayList() {
 
   const dropdownHtml = `
   <label for="country-select"></label>
-  <select id="country-select" class="center drop" aria-label="Country List">${optionsHtml.join('')}</select>
+  <select id="country-select" class="center drop" aria-label="Country List">${optionsHtml.join("")}</select>
    `;
-  $('#country-list').html(dropdownHtml);
+  $("#country-list").html(dropdownHtml);
 }
 
 //calls the webcam API and renders webcam results
 function getWebcamAPIData() {
-  $('#country-list').on('change', 'select', event => {
-    let countryKeySelected = $('#country-select').val();
+  $("#country-list").on("change", "select", event => {
+    let countryKeySelected = $("#country-select").val();
     let countryNameSelected = $("#country-select").text();
   $.ajax({
   url: `https://webcamstravel.p.mashape.com/webcams/list/limit=6/orderby=popularity,desc/category=mountain/country=${countryKeySelected}`,
-  data: {show:'webcams:image,location,player'},
-  dataType: 'json',
-  type: 'GET',
-  headers: {"X-Mashape-Key": 'cgDecNwco7mshVPaItCd9ogfs5nnp1OCnOVjsn8Yn430nwywin',
-  "X-Mashape-Host": 'webcamstravel.p.mashape.com'},
+  data: {show:"webcams:image,location,player"},
+  dataType: "json",
+  type: "GET",
+  headers: {"X-Mashape-Key": "cgDecNwco7mshVPaItCd9ogfs5nnp1OCnOVjsn8Yn430nwywin",
+  "X-Mashape-Host": "webcamstravel.p.mashape.com"},
     success: function(data) {      
       let webcamArray = data.result.webcams.map(function(webcam) {
         if (data.result.total > 0) { 
@@ -40,29 +40,28 @@ function getWebcamAPIData() {
         }
       });
       if(webcamArray.length == 0) {
-        $('#js-cam-results-text').html('');
-        $('#js-cam-results').html("<h4>This country has no webcams yet :( </h4>");
+        $("#js-cam-results-text").html("");
+        $("#js-cam-results").html("<h4>This country has no webcams yet :( </h4>");
       } else {
-        $('#js-cam-results-text').html(`<h3 id="scrollTo" class="cam-results-for">Webcam Results for ${data.result.webcams[0].location.country}</h3>`);
-        $('#js-cam-results').html(webcamArray.join(''));
+        $("#js-cam-results-text").html(`<h3 id="scrollTo" class="cam-results-for">Webcam Results for ${data.result.webcams[0].location.country}</h3>`);
+        $("#js-cam-results").html(webcamArray.join(""));
       }
     }
   });
   });
 }
-
-//calls YouTube API 
-function getDataFromAPI(callback) {
-   $('#country-list').on('change', 'select', event => {
-      let countryNameSelected = $('#country-select option:selected').text();
+ 
+function getDataFromYouTubeAPI(callback) {
+   $("#country-list").on("change", "select", event => {
+      let countryNameSelected = $("#country-select option:selected").text();
       const settings = {
-        part: 'snippet',
-        key: APIkey,
+        part: "snippet",
+        key: YOUTUBE_APIkey,
         q: `Best of travel in ${countryNameSelected}`,
         maxResults: 6,
-        relevanceLanguage: 'en',
-        type: 'video',
-        order: 'Relevance'
+        relevanceLanguage: "en",
+        type: "video",
+        order: "Relevance"
       };
       $.getJSON(YOUTUBE_SEARCH_URL, settings, callback);
     });
@@ -71,23 +70,23 @@ function getDataFromAPI(callback) {
 function generateResult(result) {
     return `
     <div class="vid-div">
-    <iframe class="vid-results" src="https://www.youtube.com/embed/${result.id.videoId}" title='${result.snippet.title}' aria-label='YouTube Video'></iframe>
+    <iframe class="vid-results" src="https://www.youtube.com/embed/${result.id.videoId}" title="${result.snippet.title}" aria-label="YouTube Video"></iframe>
     </div>`;
 }
 
 //renders YouTube results
 function displayYouTubeSearchData(data) {
-  let countryNameSelected = $('#country-select option:selected').text();
+  let countryNameSelected = $("#country-select option:selected").text();
   const results = data.items.map((item, index) => generateResult(item));
-  $('#js-results-text').html(`<h3 class="video-col-title">Video Results for ${countryNameSelected}</h3>`);
-  $('#js-results').html( results );
+  $("#js-results-text").html(`<h3 class="video-col-title">Video Results for ${countryNameSelected}</h3>`);
+  $("#js-results").html( results );
 }
 
 //scrolls to results when country is clicked
 function scrollTo() {
-$("#country-select").on('change', function() {
-  $('html, body').animate({
-      scrollTop: $('#js-cam-results').offset().top
+$("#country-select").on("change", function() {
+  $("html, body").animate({
+      scrollTop: $("#js-cam-results").offset().top
   }, 1000);
 });
 }
@@ -96,5 +95,5 @@ $(document).ready(function() {
   displayList();
   scrollTo();
   getWebcamAPIData();
-  getDataFromAPI(displayYouTubeSearchData);
+  getDataFromYouTubeAPI(displayYouTubeSearchData);
 });
